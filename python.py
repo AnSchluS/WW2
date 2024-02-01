1eac6234a18671f182140cd0b620fa368246d9f2
AssayyidJamiil <158445393+AssayyidJamiil@users.noreply.github.com>
2024-02-01 09:19:03 +0100

    dijhadist-python

---
+++ python.py
@@ -0,0 +1,28 @@
+print("hello, how are you")
+name = input("podaj swoje imie ")
+print("witaj" + name)
+print() 
+print("Witaj", name)
+liczba = 5 
+city = "Warsaw"
+print(city)
+city=125
+print(city)
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, y, z)
+
+x ,y ,z = "Orange" ,"Banana" ,"Cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="Orange"
+print(x)
+print(y)
+print(z)
+x="pyton"
+y="kobra"
+z="tiger"
+print(x,y,z)
+print(x+y+z)
